e10f520e39a824084724f84061b86fd170c93584
sleex89 <samlee723@gmail.com>
2018-06-27 15:25:59 -0700

    Top portion of header

--- client/src/components/Home/Header/Header.js
+++ client/src/components/Home/Header/Header.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import styles from './Header.css';
+import Logo from '../../../img/logo_trans_yellow.png';
 
 const Header = (props) => {
   return (
-    <div></div>
+    <div className={styles.Header}>
+      {/* <div className={styles.overlay}></div> */}
+      <div className={styles.HeaderContent}>
+        <div className={styles.Message}>WELCOME!</div>
+        <a href="index.html">
+          <img src={Logo} alt=""/>
+        </a>
+        <div className={styles.Message__two}>Virtual Golf Tournaments</div>
+      </div>
+      <hr/>
+      <div className={styles.HeaderBottom}>
+        <div className={styles.BottomMessage}>Brought to you by</div>
+        <img src="https://cdn3.bigcommerce.com/s-e2q6mh3/product_images/uploaded_images/footer-logo.png?t=1499899063&_ga=2.221937624.1159005451.1499873479-1215408688.1499873479"/>
+      </div>
+    </div>
   )
 }
 

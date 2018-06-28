31ca90624253317c8f3513373d46afe67224c9cd
sleex89 <samlee723@gmail.com>
2018-06-28 15:24:31 -0700

    Finished styling Rules section

--- client/src/components/Home/Rules/Rules.js
+++ client/src/components/Home/Rules/Rules.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './Rules.css';
 import FontAwesome from 'react-fontawesome';
-
+import { Table } from 'react-bootstrap';
 
 const rules = () => {
   return(
     <div className={styles.Rules}>
       <div className={styles.Header}>
-        <div className={styles.HeaderText}>Rules</div>
+        <h1 className={styles.HeaderText}>Rules</h1>
         <hr/>
       </div>
       <div className={styles.RulesContent}>
@@ -19,8 +19,9 @@ const rules = () => {
             size='3x'
             style={{ color: '#f74b38' }}
             />
-            <div className={styles.RuleTitle}>Overview</div>
-            <div>3-month Tournament 30 Players</div>
+            <h2 className={styles.RuleTitle}>Overview</h2>
+            <p>3-month Tournament</p>
+            <p>30 Players</p>
           </div>
           <div className={styles.Rule}>
             <FontAwesome
@@ -29,8 +30,20 @@ const rules = () => {
             size='3x'
             style={{ color: '#f74b38' }}
             />
-            <div className={styles.RuleTitle}>Tournament</div>
-            <div>3-month Tournament 30 Players</div>
+            <h2 className={styles.RuleTitle}>Tournament</h2>
+            <p>3-month Tournament</p>
+            <p>30 Players</p>
+          </div>
+          <div className={styles.Rule}>
+            <FontAwesome
+            className='fa-battery-quarter'
+            name='education'
+            size='3x'
+            style={{ color: '#f74b38' }}
+            />
+            <h2 className={styles.RuleTitle}>Pausing Subscription</h2>
+            <p>3-month Tournament</p>
+            <p>30 Players</p>
           </div>
         </div>
         <div className={styles.RulesRow}>
@@ -41,19 +54,86 @@ const rules = () => {
             size='3x'
             style={{ color: '#f74b38' }}
             />
-            <div className={styles.RuleTitle}>Points System</div>
-            <div>3-month Tournament 30 Players</div>
+          <h2 className={styles.RuleTitle}>Points System</h2>
+            <Table responsive>
+              <thead>
+                <tr>
+                  <th>Placement</th>
+                  <th>Points</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>14,706</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>10,023</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>8,900</td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>6,727</td>
+                </tr>
+                <tr>
+                  <td>5</td>
+                  <td>4,755</td>
+                </tr>
+                <tr>
+                  <td>6</td>
+                  <td>3,721</td>
+                </tr>
+              </tbody>
+            </Table>
           </div>
           <div className={styles.Rule}>
             <FontAwesome
-            className='fa-battery-quarter'
+            className='fa-times-circle'
             name='education'
             size='3x'
             style={{ color: '#f74b38' }}
             />
-            <div className={styles.RuleTitle}>Pausing Subscription</div>
-            <div>3-month Tournament 30 Players</div>
+            <h2 className={styles.RuleTitle}>Prize Pool</h2>
+            <Table responsive>
+              <thead>
+                <tr>
+                  <th>Placement</th>
+                  <th>Points</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>$1,000</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>$400</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>$300</td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>$2507</td>
+                </tr>
+                <tr>
+                  <td>5</td>
+                  <td>$200</td>
+                </tr>
+                <tr>
+                  <td>6</td>
+                  <td>$150</td>
+                </tr>
+              </tbody>
+            </Table>
           </div>
+          
         </div>
       </div>
     </div>

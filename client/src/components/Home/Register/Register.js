72f68c096d7b90aa0fd70bf8f6a67eca2962140d
sleex89 <samlee723@gmail.com>
2018-06-28 15:27:25 -0700

    Created register component

---
+++ client/src/components/Home/Register/Register.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './Register.css';
+
+class Register extends Component {
+  render() {
+    return(
+      <div>Register</div>
+    )
+  }
+}
+
+export default Register;
